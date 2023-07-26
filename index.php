89f17642e1abee338a784a1ae4dd413b41c1b203
Vjfrib <107771171+Vjfrib@users.noreply.github.com>
2023-07-26 00:03:48 +0000

    Teste

--- index.php
+++ index.php
@@ -5,5 +5,15 @@
   <body>
     <?php echo '<p>Hello World' ?>
     <script>alert("pegando")</script>
+    como disse luis: tudo é escrito
+    loren ipsum
+    ele não é uma lingguagem de programação pra console.
+    ela é uma linguagem... que faz processo em documentos. vc pode escrever qualquer besteira que vai ser exibida, como uma marcação
+    vc processa em pontos específicos.
+    <?php
+      echo 'ja... aqui pra exibir...'
+      echo 'tem que ser feito com o echo e talvez a tag...'
+    ?>
+    
   </body>
 </html> 
